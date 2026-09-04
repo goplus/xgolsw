bf00cf1f7c1c7f1dcb439a745de231af1f5a3c71
wuxinyi <wuxinyi@qiniu.com>
2026-09-04 15:55:14 +0800

    refactor: use generic class info callback name

--- internal/server/format.go
+++ internal/server/format.go
@@ -107,7 +107,7 @@ func (s *Server) formatSpxXGo(snapshot *xgo.Project, spxFile string) ([]byte, er
 		return nil, fs.ErrNotExist
 	}
 	original := file.Content
-	formatted, err := format.Source(original, spxClassInfo, spxFile)
+	formatted, err := format.Source(original, classInfo, spxFile)
 	if err != nil {
 		return nil, err
 	}
@@ -431,11 +431,11 @@ func (s *Server) formatSpxDecls(snapshot *xgo.Project, spxFile string) ([]byte, 
 	if len(formatted) == 0 || string(formatted) == "\n" {
 		return []byte{}, nil
 	}
-	return format.Source(formatted, spxClassInfo, spxFile)
+	return format.Source(formatted, classInfo, spxFile)
 }
 
-// spxClassInfo provides class information when formatting SPX source files.
-func spxClassInfo(string) (autoLambdas map[string]int, isProj, ok bool) {
+// classInfo is the parser class information used when formatting SPX files.
+func classInfo(string) (autoLambdas map[string]int, isProj, ok bool) {
 	return nil, true, true
 }
 
